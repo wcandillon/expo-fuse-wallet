3ef7ee01d23b28e44d141452fc26187f8ff483bf
William Candillon <wcandillon@gmail.com>
2019-09-01 13:42:47 +0200

    🤷🏼‍♂️

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+{
+  "presets": ["babel-preset-expo"],
+  "env": {
+    "development": {
+      "plugins": ["transform-react-jsx-source"]
+    }
+  },
+	"plugins": [
+   ["module-resolver", {
+      "alias": {
+        "randombytes": "./randombytes",
+        "crypto": "crypto-browserify"
+      }
+    }]
+  ]
+
+}
